77b48a5b177964878464cf7a6f6e37a04d43fd86
Michael Lescisin <michael.lescisin@uhn.ca>
2023-04-11 17:11:19 -0400

    Add the heracles_env.sh script for configuring the environment variables for monitoring a Cards4Heracles instance for failed nightly S3 data exports

---
+++ heracles_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export ERROR_LOG_TEXT="io.uhndata.cards.heracles.internal.export.ExportTask Failed to perform the nightly export"
+export MONITORED_CONTAINER_PRETTY_NAME="CARDS - Heracles"

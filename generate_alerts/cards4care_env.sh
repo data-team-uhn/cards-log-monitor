333ca88bd6d62687a3cd8fd89a0ae2dd0c39218b
Michael Lescisin <michael.lescisin@uhn.ca>
2023-05-11 18:22:20 -0400

    Add the cards4care environment configuration file

---
+++ generate_alerts/cards4care_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ERROR_LOG_TEXT="ca.sickkids.ccm.lfs.cardiacrehab.internal.export.ExportTask Failed to perform the nightly export"
+export SUCCESS_SUBJECT_EXPORT_TEXT="ca.sickkids.ccm.lfs.cardiacrehab.internal.export.ExportTask Exported /Subjects/"
+export SUCCESS_FORM_EXPORT_TEXT="ca.sickkids.ccm.lfs.cardiacrehab.internal.export.ExportTask Exported /Forms/"
+export MONITORED_CONTAINER_PRETTY_NAME="CARDS - Cards4CaRe"
